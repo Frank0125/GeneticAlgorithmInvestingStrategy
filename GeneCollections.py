e8ef0cf788fc555b1e4f0fe1a503b637615271c7
Frank0125 <a01252974@tec.mx>
2026-04-14 23:27:54 -0700

    feat: Decorator design pattern added to Stock to centralized risk and performance values

--- GeneCollections.py
+++ GeneCollections.py
@@ -1,6 +1,6 @@
 from typing import Dict, List
 
-from classes.Stock import Risk, Stock
+from classes.Stock import Risk, SimpleStock, Stock
 
 #*Stop Loss
 # porcentage over the total invested to limit and sell
@@ -11,10 +11,10 @@ ST_LS_P_GENES : List[int] = [10,15,20,25,30,35,40,45,50,100]
 ST_LS_N_GENES : List[int]= [15,20,25,35,45,55,65,75,85,100]
 
 #* Declaring Type of Stock
-TechStock : Stock = Stock(8, 8)
-HealthStock : Stock = Stock(3, 2)
-ServicesStock : Stock = Stock(5, 4)
-FinanceStock : Stock = Stock(6, 6)
+TechStock : Stock = SimpleStock(8, 8)
+HealthStock : Stock = SimpleStock(3, 2)
+ServicesStock : Stock = SimpleStock(5, 4)
+FinanceStock : Stock = SimpleStock(6, 6)
 
 TYPE_GENES : List[Stock] = [TechStock, HealthStock, ServicesStock, FinanceStock]
 
@@ -23,7 +23,6 @@ NoRisk : Risk = Risk(0)
 LowRisk : Risk = Risk(5)
 MediumRisk : Risk = Risk(10)
 HighRisk : Risk = Risk(20)
-
 RISK_GENES : List[Risk]= [NoRisk, LowRisk, MediumRisk, HighRisk]
 
 
--- classes/InvestStrategy.py
+++ classes/InvestStrategy.py
@@ -1,24 +1,23 @@
 import random
 from abc import ABC, abstractmethod
-from classes.Stock import Risk, Stock
+from classes.Stock import Risk, RiskDecorator, Stock
 
 from GeneCollections import ST_LS_N_GENES, ST_LS_P_GENES, RISK_GENES, TYPE_GENES, SCHEDULE_GENES
 
 RISE = [-1,1]
 
 class InvestStrategy():
-    def __init__(self, chosen_stock = TYPE_GENES[0], schedule = 0): #all parameters are genes
-        self.chosen_stock : Stock = chosen_stock #Stock(performance = 0, sector_risk = 0)
-        self.extra_risk : Risk = Risk(0)
+    def __init__(self, stock_gene = TYPE_GENES[0], risk_gene = RISK_GENES[0], schedule = 0): #all parameters are genes
+        self.chosen_stock : Stock = RiskDecorator(stock_gene, risk_gene) #to the stock we add the values added from risk gene, now chosen_stock has total risk and performance
         self.schedule : int = schedule
         self.net_worth : float = 1000
         self.rise : int = 0 # decides if the stock will rise or not. 0 = drop 1 = rise
-        self.total_risk = self.chosen_stock.get_risk() + self.extra_risk.get_added_risk()
+        # self.total_risk = self.chosen_stock.get_risk() + self.extra_risk.get_added_risk() #! no longer need
         
 
     def rise_calculation(self) -> None:
         random_num = random.randint(0,100) #different everytime
-        risk_calc : float = (random_num + self.total_risk) / 100
+        risk_calc : float = (random_num + self.chosen_stock.get_risk()) / 100
         rise_trenchmark : float = 0.5
         print(risk_calc)
         if risk_calc > rise_trenchmark: #when risk calculation is over trenchmark, rise gets set to 0
@@ -34,7 +33,7 @@ class InvestStrategy():
     def growth(self) -> None:
         self.rise_calculation()
         print(RISE[self.rise])
-        total_yield = (self.chosen_stock.get_performance() + self.extra_risk.get_bonus_performance()) / 100
+        total_yield = (self.chosen_stock.get_performance()) / 100
         self.net_worth *= (1 + (total_yield * RISE[self.rise]))
         self.check_net_worth()
         return
